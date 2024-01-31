71de0aa092e15818d93785dc1178e78a4fd6c377
pyunghun <jjg524@naver.com>
2024-01-31 17:26:10 +0900

    [test2] test 2

--- src/main/java/com/ohgiraffers/section01/literal/Applicatoin03.java
+++ src/main/java/com/ohgiraffers/section01/literal/Applicatoin03.java
@@ -14,7 +14,16 @@ public class Applicatoin03 {
 
         System.out.println('1' - 1);
 
+        /* 목차 2 세 개의 문자열 합치기*/
+        System.out.println("======================세 개의 문자열 합치기========================");
+        System.out.println(9 + 9 + "9");
+        System.out.println(9 + "9" + 9);
+        System.out.println("9" + 9 + 9); //이 결과를 918로 만들고싶다
+        System.out.println("9" + (9 + 9));
 
+        System.out.println("==================10과 20의 사칙연산 결과 보기 좋게 출력하기===============");
+        System.out.println("10과 20의 합 :" + 10 + 20);   // 괄호를 묶지 않으면 문자열 합치기가 일어나서 결과가 1020이 된다.
+        System.out.println("10과 20의 합 :" + (10 + 20));
 
 
 
